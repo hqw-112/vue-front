a706e5fb88611f328bd2f78db19ee83c5a7e4240
hqw-112 <836861836@qq.com>
2022-05-29 02:15:45 +0800

    f

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
-  <ul>
-    <li v-for="(user) in users" :key="user.id">{{ user.name }}</li>
-  </ul>
+  <ul><li v-for="(user) in users" :key="user.id">{{user.name}}</li></ul>
 </template>
 
 <script>
@@ -9,16 +7,16 @@ import axios from 'axios'
 
 export default {
   name: 'App',
-  data () {
-    return {
-      users: []
-    }
-  }, mounted () {
-    axios.get('/api/users').then(res => {
-      this.users = res.data;
-      console.log(res.data)
-    })
+ data(){
+  return{
+    users:[]
   }
+ },mounted(){
+   axios.get('http://localhost:3000/api/users').then(res=>{
+     this.users=res.data;
+     console.log(res.data)
+   })
+ }
 }
 </script>
 
